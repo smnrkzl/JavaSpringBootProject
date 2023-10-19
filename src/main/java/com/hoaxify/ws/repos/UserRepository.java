3244be89628c92b2a41e1868e70221d7b12b071e
smnrkzl <kizilsemanur@gmail.com>
2023-10-19 14:48:26 +0300

    add UserRepository

---
+++ src/main/java/com/hoaxify/ws/repos/UserRepository.java
@@ -0,0 +1,27 @@
+package com.hoaxify.ws.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hoaxify.ws.entities.Students;
+import com.hoaxify.ws.entities.Users;
+
+public interface UserRepository extends JpaRepository<Users,Long>{
+
+List<Users> findByUserId(Long userId) ;
+	
+	List<Users> findByUserNameSurname(String nameSurname);
+	
+	List<Users> findByMail(String mail);
+	
+	Users findByPhoneNumber(String phoneNumber);
+	
+	List<Users> findByRegistrationDate(String registrationdate);
+	
+	List<Users> findByRole (String role);
+	
+	List<Users> findByUpdate (String update);
+	
+	
+}
