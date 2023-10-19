da1867992b7db2f0e8b91b06deb36819a117091d
smnrkzl <kizilsemanur@gmail.com>
2023-10-19 15:10:27 +0300

    add InstructorRepository

---
+++ src/main/java/com/education/ws/repos/InstructorRepository.java
@@ -0,0 +1,34 @@
+package com.education.ws.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.education.ws.entities.Instructor;
+
+
+
+
+
+
+public interface InstructorRepository extends JpaRepository<Instructor,Long> {
+	
+List<Instructor> findByInstructorId(Long userId) ;
+	
+	List<Instructor> findByName(String name);
+	
+	List<Instructor> findByMail(String mail);
+	
+	Instructor findByCourses(String courses);
+	
+	List<Instructor> findByNotifications(String notifications);
+	
+	List<Instructor> findByInsGroup (String group);
+	
+	List<Instructor> findByPassword (String password);
+	
+	
+}
+
+
+
