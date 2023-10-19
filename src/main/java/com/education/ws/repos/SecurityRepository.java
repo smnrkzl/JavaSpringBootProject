6e014a4c626e747e40c47c8d951089edba30203a
smnrkzl <kizilsemanur@gmail.com>
2023-10-19 15:16:58 +0300

    add SecurityRepository

---
+++ src/main/java/com/education/ws/repos/SecurityRepository.java
@@ -0,0 +1,21 @@
+package com.education.ws.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.education.ws.entities.Security;
+import com.education.ws.entities.Students;
+
+public interface SecurityRepository extends JpaRepository<Security,Long> {
+
+List<Security> findBySecurityId(Long userId) ;
+	
+	List<Security> findByMove(String move);
+	
+	List<Security> findByUser(String user);
+	
+	
+}
+
