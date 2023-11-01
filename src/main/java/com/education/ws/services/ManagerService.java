a5e66ef4de03f9df920acfc3654284b10f01ab0c
smnrkzl <kizilsemanur@gmail.com>
2023-11-01 14:45:41 +0300

    addExamRepositoryInManagerService

--- src/main/java/com/education/ws/services/ManagerService.java
+++ src/main/java/com/education/ws/services/ManagerService.java
@@ -1,8 +1,17 @@
 package com.education.ws.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.education.ws.repos.ExamRepository;
 
 @Service
 public class ManagerService {
 
+	@Autowired
+	private ExamRepository examRepository;
+	
+	
+	
+	
 }
