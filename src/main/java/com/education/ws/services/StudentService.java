904d73284e3e57b1fdd1dbb9f9b10932ccc9077e
smnrkzl <kizilsemanur@gmail.com>
2023-11-01 14:50:07 +0300

    addExamRepositoryInStudentService

--- src/main/java/com/education/ws/services/StudentService.java
+++ src/main/java/com/education/ws/services/StudentService.java
@@ -1,8 +1,17 @@
 package com.education.ws.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.education.ws.repos.ExamRepository;
 
 @Service
 public class StudentService {
 
+	@Autowired
+	private ExamRepository examRepository;
+	
+	
+	
+	
 }
