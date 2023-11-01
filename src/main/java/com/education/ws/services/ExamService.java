de00c206408eb038a42a91452d8e541534149eea
smnrkzl <kizilsemanur@gmail.com>
2023-11-01 15:08:46 +0300

    addExamRepositoryInExamService

--- src/main/java/com/education/ws/services/ExamService.java
+++ src/main/java/com/education/ws/services/ExamService.java
@@ -1,10 +1,13 @@
 package com.education.ws.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Service;
 
+import com.education.ws.entities.Exam;
 import com.education.ws.repos.ExamBookletRepository;
 import com.education.ws.repos.ExamCalendarRepository;
+import com.education.ws.repos.ExamRepository;
 
 @Service
 public class ExamService {
@@ -12,11 +15,15 @@ public class ExamService {
 	@Autowired
 	private ExamBookletRepository examBookletRepository;
 	
+	@Autowired
+	private ExamRepository examRepository;
 	
 	@Autowired
 	private ExamCalendarRepository examCalendarRepository;
 	
-	
+	/*public Exam getById(Long id) {
+		return 
+	}*/
 	
 	
 }
