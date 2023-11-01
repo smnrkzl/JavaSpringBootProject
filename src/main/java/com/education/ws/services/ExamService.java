4e5b0cb05d36fcb0f661b4ec3590f2542d19c857
smnrkzl <kizilsemanur@gmail.com>
2023-11-01 14:42:04 +0300

    addExamBookletRepositoryInExamService

--- src/main/java/com/education/ws/services/ExamService.java
+++ src/main/java/com/education/ws/services/ExamService.java
@@ -1,8 +1,18 @@
 package com.education.ws.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.education.ws.repos.ExamBookletRepository;
 
 @Service
 public class ExamService {
 
+	@Autowired
+	private ExamBookletRepository examBookletRepository;
+	
+	
+	
+	
+	
 }
