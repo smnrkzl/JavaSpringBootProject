b82fe349149367103806e9efd295cfd7248ff526
smnrkzl <kizilsemanur@gmail.com>
2023-11-01 15:44:43 +0300

    addStudentRepositoryInStudentService

--- src/main/java/com/education/ws/services/StudentService.java
+++ src/main/java/com/education/ws/services/StudentService.java
@@ -6,12 +6,17 @@ import org.springframework.stereotype.Service;
 import com.education.ws.repos.CoursesRepository;
 import com.education.ws.repos.ExamRepository;
 import com.education.ws.repos.GroupsRepository;
+import com.education.ws.repos.StudentRepository;
 
 @Service
 public class StudentService {
 
 	@Autowired
 	private ExamRepository examRepository;
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
 	
 	@Autowired
 	private GroupsRepository groupsRepository;
