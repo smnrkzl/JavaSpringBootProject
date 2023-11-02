5eb1bdab147f137d89ea6f079c3f882adc9cda90
smnrkzl <kizilsemanur@gmail.com>
2023-11-03 00:47:58 +0300

    addCoursesDeleteInManagerService

--- src/main/java/com/education/ws/services/ManagerService.java
+++ src/main/java/com/education/ws/services/ManagerService.java
@@ -3,7 +3,9 @@ package com.education.ws.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.education.ws.entities.Courses;
 import com.education.ws.entities.Exam;
+import com.education.ws.entities.ExamBooklet;
 import com.education.ws.entities.Manager;
 import com.education.ws.repos.CoursesRepository;
 import com.education.ws.repos.ExamRepository;
@@ -45,4 +47,9 @@ public class ManagerService {
 	public Manager updateNameMail(String name,  String mail) {
 		   return managerRepository.update(name,mail);
 		  }
+	
+	public void CoursesDelete(Courses courses) {
+		coursesRepository.delete(courses); 
+	}
+	
 }
