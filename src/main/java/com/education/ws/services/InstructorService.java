596afb9e484b7cbeac92f8eaf4fad58e29bc307c
smnrkzl <kizilsemanur@gmail.com>
2023-11-03 16:41:22 +0300

    addNotificationsRepositoryInInstructorService

--- src/main/java/com/education/ws/services/InstructorService.java
+++ src/main/java/com/education/ws/services/InstructorService.java
@@ -3,11 +3,15 @@ package com.education.ws.services;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.education.ws.repos.CoursesRepository;
+import com.education.ws.repos.NotificationsRepository;
 
 public class InstructorService {
 
 	@Autowired
 	private CoursesRepository coursesRepository;
+	
+	@Autowired
+	private NotificationsRepository notificationsRepository;
 	
 	
 	
