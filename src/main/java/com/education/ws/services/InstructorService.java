43d726dc50cfa6c19ebe53f75e043b716ab36259
smnrkzl <kizilsemanur@gmail.com>
2023-11-03 16:44:17 +0300

    addInstructorRepositoryInInstructorService

--- src/main/java/com/education/ws/services/InstructorService.java
+++ src/main/java/com/education/ws/services/InstructorService.java
@@ -2,8 +2,11 @@ package com.education.ws.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.education.ws.entities.Exam;
+import com.education.ws.entities.Instructor;
 import com.education.ws.repos.CoursesRepository;
 import com.education.ws.repos.GroupsRepository;
+import com.education.ws.repos.InstructorRepository;
 import com.education.ws.repos.NotificationsRepository;
 
 public class InstructorService {
@@ -17,8 +20,12 @@ public class InstructorService {
 	@Autowired
 	private GroupsRepository groupsRepository;
 	
+	@Autowired
+	private InstructorRepository ınstructorRepository;
 	
-	
+	/*public Instructor getById(Long id) {
+		return .findById(id).get(); 
+	}*/
 	
 	
 }
