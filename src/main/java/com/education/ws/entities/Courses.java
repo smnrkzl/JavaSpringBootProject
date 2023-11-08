87ace55f7e5c880b08b9d00413d65507178e6a40
smnrkzl <kizilsemanur@gmail.com>
2023-11-08 09:37:26 +0300

    updateCourses

--- src/main/java/com/education/ws/entities/Courses.java
+++ src/main/java/com/education/ws/entities/Courses.java
@@ -1,9 +1,16 @@
 package com.education.ws.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+
+@Entity
+@Table(name="courses")
+@Data
 public class Courses {
 	
 	@Id
