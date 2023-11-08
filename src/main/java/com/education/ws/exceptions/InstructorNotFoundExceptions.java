bcf9e55cc985f48e7625847e0d1d7fd5f51d7f2c
smnrkzl <kizilsemanur@gmail.com>
2023-11-08 11:14:51 +0300

    addInstructorNotFoundExceptions

---
+++ src/main/java/com/education/ws/exceptions/InstructorNotFoundExceptions.java
@@ -0,0 +1,28 @@
+package com.education.ws.exceptions;
+
+public class InstructorNotFoundExceptions extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	//private static final long serialVersionUID = 1L;
+
+
+
+	public InstructorNotFoundExceptions() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public InstructorNotFoundExceptions(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	
+	
+}
