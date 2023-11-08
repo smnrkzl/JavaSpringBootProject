e13a1a9066d4a7a9f2d01d406ceb68bade3ffe94
smnrkzl <kizilsemanur@gmail.com>
2023-11-08 09:56:38 +0300

    addExamController

---
+++ src/main/java/com/education/ws/controller/ExamController.java
@@ -0,0 +1,13 @@
+package com.education.ws.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/exam")
+@AllArgsConstructor
+public class ExamController {
+
+}
