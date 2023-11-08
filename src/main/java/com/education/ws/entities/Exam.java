919a3205fcc8597e21cbdc5b8cf23bc9c42c5653
smnrkzl <kizilsemanur@gmail.com>
2023-11-08 09:38:08 +0300

    updateExam

--- src/main/java/com/education/ws/entities/Exam.java
+++ src/main/java/com/education/ws/entities/Exam.java
@@ -8,9 +8,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name="exam")
+@Data
 public class Exam {
     
 	@Id
