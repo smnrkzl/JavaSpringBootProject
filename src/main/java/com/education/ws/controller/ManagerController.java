cc6b92419a5039bd4d7fc73bcfc4dbf8e56aa6d4
smnrkzl <kizilsemanur@gmail.com>
2023-11-08 10:05:46 +0300

    addManagerServiceInManagerController

--- src/main/java/com/education/ws/controller/ManagerController.java
+++ src/main/java/com/education/ws/controller/ManagerController.java
@@ -3,6 +3,8 @@ package com.education.ws.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.education.ws.services.ManagerService;
+
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -10,4 +12,6 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ManagerController {
 
+	private ManagerService managerService;
+	
 }
