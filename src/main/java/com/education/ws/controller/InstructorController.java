3dcd8cc64091aa661bc044a37a4d0045f209f310
smnrkzl <kizilsemanur@gmail.com>
2023-11-08 09:57:26 +0300

    addInstructorController

---
+++ src/main/java/com/education/ws/controller/InstructorController.java
@@ -0,0 +1,13 @@
+package com.education.ws.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/instructor")
+@AllArgsConstructor
+public class InstructorController {
+
+}
