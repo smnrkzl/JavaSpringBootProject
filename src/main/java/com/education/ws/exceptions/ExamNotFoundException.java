6204311c2331b5ac9d6c29d1bcbc46dac6774f52
smnrkzl <kizilsemanur@gmail.com>
2023-11-08 10:55:11 +0300

    addExamNotFoundException

---
+++ src/main/java/com/education/ws/exceptions/ExamNotFoundException.java
@@ -0,0 +1,26 @@
+package com.education.ws.exceptions;
+
+public class ExamNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	//private static final long serialVersionUID = 1L;
+
+
+	public ExamNotFoundException() {
+		super();
+		
+	}
+
+	
+	public ExamNotFoundException(String message) {
+		super(message);
+		
+	}
+
+	
+
+	
+	
+}
