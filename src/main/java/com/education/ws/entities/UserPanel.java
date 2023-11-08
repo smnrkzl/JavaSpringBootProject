ff6afa604ba98463bf0d0a2949615b6daf342f4b
smnrkzl <kizilsemanur@gmail.com>
2023-11-08 09:47:31 +0300

    updateUserPanel

--- src/main/java/com/education/ws/entities/UserPanel.java
+++ src/main/java/com/education/ws/entities/UserPanel.java
@@ -1,9 +1,15 @@
 package com.education.ws.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Table(name="userpanel")
+@Data
 public class UserPanel {
    
 	@Id
